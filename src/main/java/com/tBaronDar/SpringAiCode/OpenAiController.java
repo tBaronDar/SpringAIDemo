d25d3e7adb6fb804b6a12df1b87dbd8a704733d3
Themis Darelis <thedarelis@gmail.com>
2026-04-17 12:15:48 +0300

    switched to chat client added response entity

--- src/main/java/com/tBaronDar/SpringAiCode/OpenAiController.java
+++ src/main/java/com/tBaronDar/SpringAiCode/OpenAiController.java
@@ -1,23 +1,34 @@
 package com.tBaronDar.SpringAiCode;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OpenAiController {
-    private OpenAiChatModel chatModel;
+
+    //generally ChatModel has limited functionality
+    //so we are going to use chatClient
+    //private OpenAiChatModel chatModel;
+
+    private ChatClient chatClient;
 
     public OpenAiController(OpenAiChatModel openAiChatModel) {
-        this.chatModel = openAiChatModel;
+        this.chatClient=ChatClient.create(openAiChatModel);
     }
 
     @GetMapping("api/{message}")
-    public String test(@PathVariable String message) {
+    public ResponseEntity<String> test(@PathVariable String message) {
 
-        String response = chatModel.call(message);
-        return "Jello" + response;
+        String response = chatClient
+                .prompt(message)
+                .call()
+                .content();
+
+        return ResponseEntity.ok(response);
     }
 
 }
