f95b87451e4fdc74a06b30f9a561333fdd3dd1b9
Themis Darelis <thedarelis@gmail.com>
2026-04-30 22:52:40 +0300

    configured pgvector with spring

--- src/main/java/com/tBaronDar/SpringAiCode/AppConfig.java
+++ src/main/java/com/tBaronDar/SpringAiCode/AppConfig.java
@@ -1,10 +1,11 @@
 package com.tBaronDar.SpringAiCode;
 
 import org.springframework.ai.embedding.EmbeddingModel;
-import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class AppConfig {
@@ -12,9 +13,10 @@ public class AppConfig {
   // we use the SimpleVectorStore that needs
   // the embeddingModel
   @Bean
-  public VectorStore vectorStore(EmbeddingModel embeddingModel) {
-    return SimpleVectorStore
-        .builder(embeddingModel)
+  public VectorStore vectorStore(JdbcTemplate jdbcTemplate, EmbeddingModel embeddingModel) {
+    return PgVectorStore
+        .builder(jdbcTemplate, embeddingModel)
+        .dimensions(1536)
         .build();
   }
 
