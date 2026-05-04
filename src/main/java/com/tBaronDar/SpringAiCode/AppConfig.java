a463a02c42f4a42815aa599b601e419ae6fdd883
Themis Darelis <thedarelis@gmail.com>
2026-05-04 10:10:25 +0300

    added PgVectorStore options and comments

--- src/main/java/com/tBaronDar/SpringAiCode/AppConfig.java
+++ src/main/java/com/tBaronDar/SpringAiCode/AppConfig.java
@@ -3,6 +3,7 @@ package com.tBaronDar.SpringAiCode;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore.PgDistanceType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -16,7 +17,10 @@ public class AppConfig {
   public VectorStore vectorStore(JdbcTemplate jdbcTemplate, EmbeddingModel embeddingModel) {
     return PgVectorStore
         .builder(jdbcTemplate, embeddingModel)
-        .dimensions(1536)
+        .dimensions(1536)// better to specify dimensions
+        .distanceType(PgDistanceType.COSINE_DISTANCE)// this is optional
+        .indexType(PgVectorStore.PgIndexType.HNSW)// this is optional
+
         .build();
   }
 
